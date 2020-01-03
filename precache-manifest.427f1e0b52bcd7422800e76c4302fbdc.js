4b55bc97967075cdb6e732ac5006b6684e7cf5fd
Roy <cjh19891018@gmail.com>
2020-01-03 15:50:51 +0800

    Updates

---
+++ precache-manifest.427f1e0b52bcd7422800e76c4302fbdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41f924e58a993173f535d9987c3e383",
+    "url": "/draggable-markers-ts/index.html"
+  },
+  {
+    "revision": "9aadbb46bd5cbbe51231",
+    "url": "/draggable-markers-ts/static/css/main.1a9f5c44.chunk.css"
+  },
+  {
+    "revision": "ed5d712d0286b78b5c91",
+    "url": "/draggable-markers-ts/static/js/2.639f9441.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/draggable-markers-ts/static/js/2.639f9441.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9aadbb46bd5cbbe51231",
+    "url": "/draggable-markers-ts/static/js/main.3d88472c.chunk.js"
+  },
+  {
+    "revision": "1980bf6dcc4c472c299a",
+    "url": "/draggable-markers-ts/static/js/runtime-main.75589fa4.js"
+  },
+  {
+    "revision": "824818055480dbfe239c00544fe10161",
+    "url": "/draggable-markers-ts/static/media/map.82481805.png"
+  }
+]);
